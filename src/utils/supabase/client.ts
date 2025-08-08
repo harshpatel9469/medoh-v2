f147cb40b5383342100a25f0df5aa5224f641eaa
bhumikahi <michellebhu0411@gmail.com>
2025-08-08 13:35:41 +0530

    changes

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  "https://wzfygdjnsdgisofarvek.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6ZnlnZGpuc2RnaXNvZmFydmVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYyMjQ5NDQsImV4cCI6MjAzMTgwMDk0NH0.CClLvu7itgozewzp32cAmQaEXS7X8iRQ_VX3F9CAyxc"
 );
